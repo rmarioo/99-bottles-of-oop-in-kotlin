90994c4b876ab0f7c1717b876b47b47cd6f89ce7
Mario Russo <mario.russo@lastminute.com>
2019-05-31 08:35:15 +0200

    removed verse

--- src/main/java/com/rmarioo/bottle99/Bottle.kt
+++ src/main/java/com/rmarioo/bottle99/Bottle.kt
@@ -17,18 +17,20 @@ class Bottle {
              "Take it down and pass it around, " +
              "no more bottles of beer on the wall.\n"
          else
-         if (number ==2)
-            "2 bottles of beer on the wall, " +
-            "2 bottles of beer.\n" +
-            "Take one down and pass it around, " +
-            "1 bottle of beer on the wall.\n"
-        else
             "$number bottles of beer on the wall, " +
             "$number bottles of beer.\n" +
             "Take one down and pass it around, " +
-            "${number-1} bottles of beer on the wall.\n"
+            "${number-1} ${container(number-1)} of beer on the wall.\n"
 
     }
+
+
+    private fun container(number: Int =1): String {
+        if (number ==1)
+            return "bottle"
+        return "bottles"
+    }
+
 
     fun verses(start: Int ,end: Int): String
     {
