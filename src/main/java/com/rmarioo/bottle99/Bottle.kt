82522e0be2aed1ebad7df936d1f84d566af91104
Mario Russo <mario.russo@lastminute.com>
2019-05-31 12:01:53 +0200

    extract class

--- src/main/java/com/rmarioo/bottle99/Bottle.kt
+++ src/main/java/com/rmarioo/bottle99/Bottle.kt
@@ -5,44 +5,18 @@ class Bottle {
 
     fun verse(number: Int): String {
 
-     return "${quantity(number).capitalize()} ${container(number)} of beer on the wall, " +
-            "${quantity(number)} ${container(number)} of beer.\n" +
-                    action(number) +
-            "${quantity(successor(number))} ${container(successor(number))} of beer on the wall.\n"
+        val bottleNumber = BottleNumber(number)
+
+        val nextBottleNumber: BottleNumber = bottleNumber.successor()
+
+
+     return "${bottleNumber.quantity().capitalize()} ${bottleNumber.container()} of beer on" +
+             " the wall, " +
+            "${bottleNumber.quantity()} ${bottleNumber.container()} of beer.\n" +
+             bottleNumber.action() +
+            "${nextBottleNumber.quantity()} ${nextBottleNumber.container()} of beer on the wall.\n"
 
     }
-
-    private fun successor(number: Int): Int {
-        if (number ==0)
-            return 99
-        return number - 1
-    }
-
-    private fun action(number: Int): String {
-        if (number ==0)
-            return "Go to the store and buy some more, "
-        return "Take ${pronoun(number)} down and pass it around, "
-    }
-
-    private fun quantity(number: Int): String {
-        if (number ==0)
-            return "no more"
-        return number.toString()
-    }
-
-    private fun pronoun(number: Int =1): String {
-        if (number ==1)
-            return "it"
-        return "one"
-    }
-
-
-    private fun container(number: Int =1): String {
-        if (number ==1)
-            return "bottle"
-        return "bottles"
-    }
-
 
     fun verses(start: Int ,end: Int): String
     {
---
+++ src/main/java/com/rmarioo/bottle99/BottleNumber.kt
@@ -0,0 +1,34 @@
+package com.rmarioo.bottle99
+
+class BottleNumber(val number: Int)
+{
+    fun quantity(): String {
+        if (this.number ==0)
+            return "no more"
+        return this.number.toString()
+    }
+
+    fun container(): String {
+        if (this.number ==1)
+            return "bottle"
+        return "bottles"
+    }
+
+    fun action(): String {
+        if (this.number ==0)
+            return "Go to the store and buy some more, "
+        return "Take ${pronoun()} down and pass it around, "
+    }
+
+    private fun pronoun(): String {
+        if (this.number ==1)
+            return "it"
+        return "one"
+    }
+
+    fun successor(): BottleNumber {
+        if (number ==0)
+            return BottleNumber(99)
+        return BottleNumber(number-1)
+    }
+}
